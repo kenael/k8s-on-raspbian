b84bd93ba3703d59783c31cebc3166c0f56362a5
Andreas Martin <martinan@Andreass-iMac-2.fritz.box>
2019-04-14 10:21:50 +0200

    update my changes

--- myscripts/dashb_firstapp.sh
+++ myscripts/dashb_firstapp.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-set -e
-KUBECONFIG="${KUBECONFIG:-XX}"
-if [ ! -d ~/.kube ] || [ "${KUBECONFIG}" != "XX" ]; then
-  echo "look Howto to set configuration for kubectl" >&2
-  echo "Work with export or $HOME/.kube"
-  exit
-fi
+set +e
+source $(dirname $0)/lib/functions.sh
+kubeconfig
 echo "Create Roles and User"
-kubectl apply -f clusterrole.yml
-./createdashboarduser.sh
+$KUBECTL apply -f clusterrole.yml
+# ./createdashboarduser.sh
 echo "Deploy First APP and Dashboard"
-kubectl create --save-config -f kubernetes-dashboard-arm-head.yml
-kubectl create --save-config -f function_test.yml
+$KUBECTL create --save-config -f kubernetes-dashboard-arm-head.yml
+$KUBECTL create --save-config -f function_test.yml
--- myscripts/run_on_pi.sh
+++ myscripts/run_on_pi.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-set -e
+set +e
+source $(dirname $0)/lib/functions.sh
+kubeconfig
+
 if [[ "$(uname -m)" != "armv7l"  ]]; then
   echo "This script work only, on Rpi with Kubernetes-Master installation!" >&2
-  exit
-fi
-KUBECONFIG="${KUBECONFIG:-XX}"
-if [ ! -d ~/.kube ] || [ "${KUBECONFIG}" != "XX" ]; then
-  echo "look Howto to set configuration for kubectl" >&2
-  echo "Work with export or $HOME/.kube"
   exit
 fi
 echo "Install Waeve Network"
